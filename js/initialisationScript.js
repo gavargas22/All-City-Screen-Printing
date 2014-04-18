7f7aac2d4a319b71a2a14d06b01c7b4423ceda91
Guillermo Vargas <gavargas22@gmail.com>
2014-04-18 16:09:03 -0600

    New Direction

--- js/initialisationScript.js
+++ js/initialisationScript.js
@@ -1,1 +1,15 @@
-var Dropzone = require("dropzone");
+//var Dropzone = require("dropzone");
+
+$(document).ready(function() {
+				var wizard = $('#some-wizard').wizard({
+					keyboard : true,
+					contentHeight : 600,
+					contentWidth : 700,
+					backdrop: 'static'
+				});
+
+				$('#open-wizard').click(function(e) {
+					e.preventDefault();
+					wizard.show();
+				});
+			});
