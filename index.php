bb62a8cc61ff542c8474bec70f2dd646b969a42c
Guillermo Vargas <gavargas22@gmail.com>
2014-04-27 22:19:45 -0600

    Revert 47e4bc0..a3cd83f
    This rolls back to commit 47e4bc0bf50bfee81d724f121a601fa9c9619df4.

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php get_header(); ?>
+<div class="wrapper-class message-holder">
+  		<div class="container">
+  			<h1>Local and fast, <span style="text-decoration:underline;">no headaches</span> screen printing.</h1>
+  			<p>Don't let a computer control your next shirt, let us give you the best service experience.</p>
+  		 	<!--<p class="buttons"><a href="#" title="Submit your shirt design here" class="custom-btn custom-btn-lg custom-btn-primary" id="open-wizard"><span class="hidden-xs">Get Started</span><span class="visible-xs">Start</span></a>
+      </p>-->
+       		<button id="open-wizard" class="custom-btn custom-btn-lg custom-btn-primary">
+			Get Started
+		</button>
+  		 </div>
+  	</div>
+  	
+<section class="wrapper-class">
+  		<div class="page-subheader">
+      <div class="container">
+        <div class="social-buttons hidden-xs hidden-sm">
+          <div class="social-button social-button__twitter"><iframe id="twitter-widget-0" scrolling="no" frameborder="0" allowtransparency="true" src="http://platform.twitter.com/widgets/follow_button.1397165098.html#_=1397851522467&amp;id=twitter-widget-0&amp;lang=en&amp;screen_name=gavargas&amp;show_count=true&amp;show_screen_name=true&amp;size=m" class="twitter-follow-button twitter-follow-button" title="Twitter Follow Button" data-twttr-rendered="true" style="width: 228px; height: 20px;"></iframe></div>
+          <!--<div class="social-button social-button__github">
+            <iframe src="http://ghbtns.com/github-btn.html?user=JedWatson&amp;repo=keystone&amp;type=watch&amp;count=true" allowtransparency="true" frameborder="0" scrolling="0" width="86" height="20"></iframe>
+          </div>-->
+        </div>
+        <p>We are El Paso's best and most advanced Screen Printing business.</p>
+      </div>
+    </div>
+  	</section>
+  	
+<section class="shirt-wizard">
+  		<div class="wizard" id="some-wizard" data-title="Get Started">
+    		<div class="wizard-card" data-cardname="card1">
+        		<h3>What you will need.</h3>
+  				
+  				<h4>Before starting the process, here are some things you need to know.</h4>
+  				
+  				<h5>You will need the following</h5>
+  				
+  				<ol>
+  					<li>The artwork that you want to print in PDF or AI format.</li>
+  					<li></li>
+  				</ol>
+    			
+    			</div>
+
+    		<div class="wizard-card" data-cardname="card2">
+        		<h3>1. Select your canvas</h3>
+        		
+        		<div class="col-xs-3">
+  			<div class=""><img src="img/shirtIcon.png" /></div>
+  			<div><center>T-Shirts</center></div>
+  			
+  			</div>
+        		
+    		</div>
+    		
+    		<div class="wizard-card" data-cardname="card3">
+        		<h3>3. Upload your design.</h3>
+        		Some content
+    		</div>
+		</div>
+  	</section>
+  	<div class="wrapper-class">
+	
+	<section class="intro-section promotional-section">
+		
+
+        <div class="container">
+          <div class="section-header">
+            <h2>Easy Screen Printing</h2>
+            <p class="lead">Do you need to print some shirts quick and easy? We believe in making it simple for our clients to get their shirts quick! </p>
+          </div>
+        </div>
+        <div class="container admin-image-full"><img src="http://keystonejs.com/images/home/admin-interface.png" class="img-responsive"></div>
+		
+	</section>
+	
+</div>
+<?php get_footer(); ?>
