c7ef7a0d764474089806e8918e7c5cdcf8ac7c22
Guillermo Vargas <gavargas22@gmail.com>
2014-04-28 09:36:28 -0600

    Wordpress

--- footer.php
+++ footer.php
@@ -1,12 +1,16 @@
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    	<!-- Latest compiled and minified JavaScript -->
-	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-	<!--Add library for Dropzone.js-->
-	<script src="js/dropzone.min.js"></script>
-	<!--Initialise the site's Javascript-->
-	<script src="js/initialisationScript.js"></script>
-	<script src="js/bootstrap-wizard.min.js" type="text/javascript"></script>
-  </body>
+<script src="http://max.jotfor.ms/static/jotform.js?3.2.1589" type="text/javascript"></script>
+<script type="text/javascript">
+   JotForm.init();
+</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<!-- Latest compiled and minified JavaScript -->
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<!--Add library for Dropzone.js-->
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/dropzone.min.js"></script>
+<!--Initialise the site's Javascript-->
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/initialisationScript.js"></script>
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/bootstrap-wizard.min.js" type="text/javascript"></script>
+</body>
 </html>
