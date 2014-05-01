a52ac991ff2d5798fee2a040ff5d63ccd82d18d5
Guillermo Vargas <gavargas22@gmail.com>
2014-04-30 20:42:06 -0600

    Work Page

--- page-work.php
+++ page-work.php
@@ -1,32 +1,65 @@
 <?php get_header(); ?>
-    <div style="background-image:url(<?php bloginfo('stylesheet_directory'); ?>/images/background.png)"
-    class="wrapper-class message-holder">
-        <div class="container">
-            <h1>Work Samples</h1>
-            <p>Our team can make any project come to life, no matter how complex.</p>
-        </div>
-    </div>
-    <div class="wrapper-class">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="work-tile">
-                    <div class="work-tile-image-holder">
-                        <img src="https://dov5cor25da49.cloudfront.net/products/4459/1272x920shirt_guys_01.jpg"
-                        />
-                    </div>
-                    <div class="work-tile-description-holder">
-                        <h4>Rocket Science</h4>
-                    </div>
-                </div>
-                <div class="work-tile">
-                    <div class="work-tile-image-holder">
-                        <img src="https://dov5cor25da49.cloudfront.net/products/3997/636x460shirt_guys_01.jpg"
-                        />
-                        <div class="work-tile-description-holder">
-                            <h4>El Paso Skull</h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <?php get_footer(); ?>
+<div style="background-image:url(<?php bloginfo('stylesheet_directory'); ?>/images/background.png)" class="wrapper-class message-holder">
+	<div class="container">
+		<h1>Work Samples</h1>
+		<p>
+			Our team can make any project come to life, no matter how complex.
+		</p>
+	</div>
+</div>
+<div class="wrapper-class" style="z-index: -1">
+
+	<div class="work-tile">
+		<div class="work-tile-image-holder">
+			<img src="https://dov5cor25da49.cloudfront.net/products/5040/636x460shirt_guys_01.jpg" />
+		</div>
+		<div class="work-tile-description-holder">
+			<p>Furr Division</p>
+		</div>
+	</div>
+	<div class="work-tile">
+		<div class="work-tile-image-holder">
+			<img src="https://dov5cor25da49.cloudfront.net/products/3997/636x460shirt_guys_01.jpg" />
+			<div class="work-tile-description-holder">
+				<p>El Paso Skull</p>
+			</div>
+		</div>
+	</div>
+	
+	<div class="work-tile">
+		<div class="work-tile-image-holder">
+			<img src="https://dov5cor25da49.cloudfront.net/products/5842/636x460shirt_guys_01.jpg" />
+			<div class="work-tile-description-holder">
+				<p>Abide</p>
+			</div>
+		</div>
+	</div>
+	
+	<div class="work-tile">
+		<div class="work-tile-image-holder">
+			<img src="https://dov5cor25da49.cloudfront.net/products/5857/636x460shirt_guys_01.jpg" />
+			<div class="work-tile-description-holder">
+				<p>Front Stage</p>
+			</div>
+		</div>
+	</div>
+	
+	<div class="work-tile">
+		<div class="work-tile-image-holder">
+			<img src="https://dov5cor25da49.cloudfront.net/products/3602/636x460shirt_guys_01.jpg" />
+			<div class="work-tile-description-holder">
+				<p>Say something nice</p>
+			</div>
+		</div>
+	</div>
+	
+	<div class="work-tile">
+		<div class="work-tile-image-holder">
+			<img src="https://dov5cor25da49.cloudfront.net/products/5826/636x460shirt_guys_01.jpg" />
+			<div class="work-tile-description-holder">
+				<p>Cool</p>
+			</div>
+		</div>
+	</div>
+</div>
+<?php get_footer(); ?>
